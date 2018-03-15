664721311ff7a8e8eb248910b8543f4551cbd570
John Gratton <jgratton@gmail.com>
2018-03-15 01:23:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (let i = 1; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
     array[i] = `${musicians[i]} plays ${instruments[i]}.`;
   }
   return array;
